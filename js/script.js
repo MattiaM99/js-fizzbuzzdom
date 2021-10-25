8014f012a415893a305b9808b3273c8399a61bc8
MattiaM99 <mattiamazzoni123@gmail.com>
2021-10-25 16:40:20 +0200

    fine progetto

--- js/script.js
+++ js/script.js
@@ -6,14 +6,21 @@ for (let i = 1; i <= 100; i++) {
   box.innerHTML = i;
   row.append(box);
   console.log(box);
-
+  
   if (!(i % 3) && !(i % 5)) {
-  box.classList.add('per3-5');
+    box.classList.add('per3-5');
+    box.innerHTML = ('FizzBuzz');
   }     
   else if (!(i % 3)) {
-  box.classList.add('per3');
+    box.classList.add('per3');
+    box.innerHTML = ('Fizz');
+
   } else if (!(i % 5)) {
-  box.classList.add('per5');
+    box.classList.add('per5');
+    box.innerHTML = ('Buzz');
   } 
   console.log(i);
 };
+
+
+
